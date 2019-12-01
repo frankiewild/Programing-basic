ecb2c02a79913ba8653ff8e6e743d007562444e9
frankiewild <frankiewild18@gmail.com>
2019-12-01 16:32:27 +0200

    hello

---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            {       int 
+                Console.WriteLine((5 * 1.8).GetType());
+
+            }
+        }
+    }
+}
